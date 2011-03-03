67b112ba53f7ff3f5b002ecfbd96f63e841460d8
Richard Anaya <richard.anaya@gmail.com>
2011-03-02 20:43:52 -0600

    first commit

---
+++ src/Engine.js
@@ -0,0 +1,164 @@
+Engine = function() {
+	mvMatrix = null;
+	pMatrix = null;
+	this.gl = null;
+}
+
+Engine.prototype.initialize = function(id) {
+    var canvas = document.getElementById(id);
+    this.initGL(canvas);
+    this.initSettings();
+    this.initShaders();
+    this.initBuffers();
+    this.initTextures();
+}
+
+Engine.prototype.initSettings = function() {
+    this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    this.gl.clearDepth(1.0);
+    this.gl.enable(this.gl.DEPTH_TEST);
+    this.gl.depthFunc(this.gl.LEQUAL);
+}
+
+Engine.prototype.start = function() {
+    var me = this;
+    setInterval(function() { me.run(); }, 1000/60.0);
+}
+
+Engine.prototype.initGL = function(canvas) {
+    try {
+      this.gl = canvas.getContext("experimental-webgl");
+      this.gl.viewportWidth = canvas.width;
+      this.gl.viewportHeight = canvas.height;
+    } catch(e) {
+    }
+    if (!this.gl) {
+      alert("Could not initialise WebGL, sorry :-(");
+    }
+  }
+ 
+
+Engine.prototype.initShaders = function() {
+
+}
+
+Engine.prototype.initBuffers = function() {
+
+}
+
+Engine.prototype.initTextures = function() {
+
+}
+
+Engine.prototype.loadIdentity = function() {
+    this.mvMatrix = Matrix.I(4);
+  }
+ 
+ 
+Engine.prototype.multMatrix = function(m) {
+    this.mvMatrix = this.mvMatrix.x(m);
+  }
+ 
+ 
+Engine.prototype.mvTranslate = function(v) {
+    var m = Matrix.Translation($V([v[0], v[1], v[2]])).ensure4x4();
+    this.multMatrix(m);
+  }
+ 
+Engine.prototype.mvRotate = function(ang, v) {
+    var arad = ang * Math.PI / 180.0;
+    var m = Matrix.Rotation(arad, $V([v[0], v[1], v[2]])).ensure4x4();
+    this.multMatrix(m);
+}
+ 
+Engine.prototype.perspective = function(fovy, aspect, znear, zfar) {
+    this.pMatrix = makePerspective(fovy, aspect, znear, zfar);
+  }
+
+Engine.prototype.ortho = function(width,height) {
+    this.pMatrix = makeOrtho(0,width,height,0,0,100);
+}
+
+Engine.prototype.run = function() {
+
+}
+
+Engine.prototype.getShader = function(id) {
+    var shaderScript = document.getElementById(id);
+    if (!shaderScript) {
+      return null;
+    }
+ 
+    var str = "";
+    var k = shaderScript.firstChild;
+    while (k) {
+      if (k.nodeType == 3) {
+        str += k.textContent;
+      }
+      k = k.nextSibling;
+    }
+ 
+    var shader;
+    if (shaderScript.type == "x-shader/x-fragment") {
+      shader = this.gl.createShader(this.gl.FRAGMENT_SHADER);
+    } else if (shaderScript.type == "x-shader/x-vertex") {
+      shader = this.gl.createShader(this.gl.VERTEX_SHADER);
+    } else {
+      return null;
+    }
+ 
+    this.gl.shaderSource(shader, str);
+    this.gl.compileShader(shader);
+ 
+    if (!this.gl.getShaderParameter(shader, this.gl.COMPILE_STATUS)) {
+      alert(this.gl.getShaderInfoLog(shader));
+      return null;
+    }
+ 
+    return shader;
+  }
+ 
+Engine.prototype.createShaderProgram = function(shaders) {
+    var program = this.gl.createProgram();
+    for(var i in shaders) {
+      this.gl.attachShader(program, this.getShader(shaders[i]));
+    }
+    this.gl.linkProgram(program);
+ 
+    if (!this.gl.getProgramParameter(program, this.gl.LINK_STATUS)) {
+      alert("Could not initialise shaders");
+    }
+    return program;
+}
+
+Engine.prototype.createBuffer = function( array, itemSize, numItems ) {
+    var buffer = this.gl.createBuffer();
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, buffer);
+    this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(array), this.gl.STATIC_DRAW);
+    buffer.itemSize = itemSize;
+    buffer.numItems = numItems;
+    return buffer;
+} 
+
+Engine.prototype.handleLoadedTexture = function(texture) {
+    this.gl.bindTexture(this.gl.TEXTURE_2D, texture);
+    this.gl.pixelStorei(this.gl.UNPACK_FLIP_Y_WEBGL, true);
+    this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, this.gl.RGBA, this.gl.UNSIGNED_BYTE, texture.image);
+this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR);
+ this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE);
+ this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE);
+    this.gl.bindTexture(this.gl.TEXTURE_2D, null);
+  }
+
+Engine.prototype.loadTexture = function(url) { 
+    var me = this;
+    var texture = this.gl.createTexture();
+    texture.image = new Image();
+    texture.image.onload = function() {
+      me.handleLoadedTexture(texture)
+    }
+    texture.image.src = url;
+    return texture
+}
+
+
---
+++ src/SpriteEngine.js
@@ -0,0 +1,115 @@
+SpriteEngine = function() {
+	this.shaderProgram = null;
+	this.vMulColor = [1,1,1,1];
+}
+
+SpriteEngine.prototype = new Engine();
+SpriteEngine.prototype.constructor = SpriteEngine;
+
+SpriteEngine.prototype.initSettings = function() {
+    Engine.prototype.initSettings.call(this);
+    this.gl.enable(this.gl.BLEND);
+    this.gl.blendFunc(this.gl.SRC_ALPHA, this.gl.ONE);
+}
+
+SpriteEngine.prototype.initShaders = function() {
+    this.shaderProgram = this.createShaderProgram(["shader-fs","shader-vs"]);
+    this.gl.useProgram(this.shaderProgram);
+ 
+    this.shaderProgram.vertexPositionAttribute = this.gl.getAttribLocation(this.shaderProgram, "aVertexPosition");
+    this.gl.enableVertexAttribArray(this.shaderProgram.vertexPositionAttribute);
+    this.shaderProgram.vertexColorAttribute = this.gl.getAttribLocation(this.shaderProgram, "aVertexColor");
+    this.gl.enableVertexAttribArray(this.shaderProgram.vertexColorAttribute);
+    this.shaderProgram.textureCoordAttribute = this.gl.getAttribLocation(this.shaderProgram, "aTextureCoord");
+    this.gl.enableVertexAttribArray(this.shaderProgram.textureCoordAttribute);
+
+    this.shaderProgram.pMatrixUniform = this.gl.getUniformLocation(this.shaderProgram, "uPMatrix");
+    this.shaderProgram.mvMatrixUniform = this.gl.getUniformLocation(this.shaderProgram, "uMVMatrix");
+    this.shaderProgram.uSampler = this.gl.getUniformLocation(this.shaderProgram, "uSampler");
+    this.shaderProgram.vMulColor = this.gl.getUniformLocation(this.shaderProgram, "vMulColor");
+  }
+SpriteEngine.prototype.setMatrixUniforms = function() {
+    this.gl.uniformMatrix4fv(this.shaderProgram.pMatrixUniform, false, new Float32Array(this.pMatrix.flatten()));
+    this.gl.uniformMatrix4fv(this.shaderProgram.mvMatrixUniform, false, new Float32Array(this.mvMatrix.flatten()));
+    this.gl.uniform4fv(this.shaderProgram.vMulColor, new Float32Array(this.vMulColor));
+  } 
+
+SpriteEngine.prototype.drawSprite = function(sprite) {
+    var positionBuffer = sprite[0];
+    var colorBuffer = sprite[1];
+    var textureCoordBuffer = sprite[2];
+    var texture = sprite[3];
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, positionBuffer);
+    this.gl.vertexAttribPointer(this.shaderProgram.vertexPositionAttribute, positionBuffer.itemSize, this.gl.FLOAT, false, 0, 0);
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, colorBuffer);
+    this.gl.vertexAttribPointer(this.shaderProgram.vertexColorAttribute, colorBuffer.itemSize, this.gl.FLOAT, false, 0, 0);
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, textureCoordBuffer);
+    this.gl.vertexAttribPointer(this.shaderProgram.textureCoordAttribute, textureCoordBuffer.itemSize, this.gl.FLOAT, false, 0, 0);
+    
+    this.gl.activeTexture(this.gl.TEXTURE0);
+    this.gl.bindTexture(this.gl.TEXTURE_2D, texture);
+    this.gl.uniform1i(this.shaderProgram.samplerUniform, 0);
+    
+    this.setMatrixUniforms();
+    this.gl.drawArrays(this.gl.TRIANGLE_STRIP, 0, positionBuffer.numItems);
+}
+
+SpriteEngine.prototype.createSprite = function(width,height,spriteColor,url) {
+    var vertices = [
+         width, height, 0.0,
+         0, height, 0.0,
+         width, 0, 0.0,
+         0, 0,  0.0
+    ];
+    var vertexPositionBuffer = this.createBuffer(vertices,3,4);
+    
+    var color = [
+      spriteColor[0],spriteColor[1],spriteColor[2],spriteColor[3],
+      spriteColor[0],spriteColor[1],spriteColor[2],spriteColor[3],
+      spriteColor[0],spriteColor[1],spriteColor[2],spriteColor[3],
+      spriteColor[0],spriteColor[1],spriteColor[2],spriteColor[3]
+    ];
+    var vertexColorBuffer = this.createBuffer(color,4,4);
+	
+    var coords = [
+         1,0,
+	 0,0,
+         1,1,
+         0,1
+    ];
+    var textureCoordBuffer = this.createBuffer(coords,2,4);
+    
+    return [vertexPositionBuffer, vertexColorBuffer, textureCoordBuffer, this.loadTexture(url)];
+}
+
+SpriteEngine.prototype.move = function(x,y) {
+	this.mvTranslate([x,y,0]);
+}
+
+SpriteEngine.prototype.rotate = function(deg) {
+	this.mvRotate(deg,[0,0,1]);
+}
+
+SpriteEngine.prototype.reset = function() {
+	this.loadIdentity();
+}
+
+SpriteEngine.prototype.setTint = function(r,g,b) {
+	this.vMulColor = [r,g,b,this.vMulColor[3]];
+}
+
+SpriteEngine.prototype.setAlpha = function(a) {
+	this.vMulColor = [this.vMulColor[0],this.vMulColor[1],this.vMulColor[2],a];
+}
+
+SpriteEngine.prototype.clear = function(r,g,b) {
+	if( r != undefined ) {
+		this.gl.clearColor(r,g,b,1.0);
+	}
+    	this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+}
+
+SpriteEngine.prototype.run = function() {
+    this.gl.viewport(0, 0, this.gl.viewportWidth, this.gl.viewportHeight);
+    this.ortho(this.gl.viewportWidth,this.gl.viewportHeight);
+}
