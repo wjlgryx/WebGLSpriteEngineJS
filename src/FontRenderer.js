6e173116cfd2f8b75d2cec120cfc84c8e83ad570
Richard Anaya <richard.anaya@gmail.com>
2011-03-17 12:12:00 -0500

    adding ability to measure string

--- src/FontRenderer.js
+++ src/FontRenderer.js
@@ -37,38 +37,18 @@ FontRenderer.prototype.drawText = function(s,x,y) {
 		x+=w;
 	}
 }
-/*FontRenderer.prototype.getSprite = function(ctx,s) {
-	var canvas = document.createElement('canvas');
-	var finalWidth = 0;
-	for(var i = 0; i < s.length ; i++ ) {
-		var c = s.charCodeAt(i)-32;	
-		var sx = this.font_data[c*2];
-		var w = this.font_data[(c*2)+1]-sx;
-		finalWidth += w;
-	}
-	var h = this.font.height-1; //account for data row
-	canvas.width = finalWidth;
-	canvas.height = h;
-	var ctx = canvas.getContext('2d');
-	ctx.fillStyle = "blue";
-	ctx.fillRect(0,0,finalWidth,h);
-	var dataUrl = canvas.toDataURL("image/png");
-	var x=0;
-	var y=0;
-	for(var i = 0; i < s.length ; i++ ) {
-		var c = s.charCodeAt(i)-32;	
-		var sx = this.font_data[c*2];
-		var w = this.font_data[(c*2)+1]-sx;
-		ctx.drawImage(this.font,sx,1,w,h,x,y,w,h);
-		x+=w;
-	}
-	dataUrl = canvas.toDataURL("image/png");
 
-	var img = new Image();
-	img.src = dataUrl;
-	return img;
+FontRenderer.prototype.getStringDimensions = function(s) {
+	var w = 0;
+	for(var i = 0,len = s.length;i<len;i++ ) {
+		var c = s.charCodeAt(i)-32;
+		if( c >= 0 && c < this.font_data.length ) {
+			w += this.font_data[(c*2)+1]-this.font_data[c*2];
+		}			
+	}
+	return {width:w,height:this.font.height};
 }
-*/
+
 FontRenderer.prototype.renderCharacter = function(sx,sy,w,h,x,y) {
 	var sw = this.sprite.texture.image.width;
 	var sh = this.sprite.texture.image.height;
