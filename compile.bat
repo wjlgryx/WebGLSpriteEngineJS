ffd988f412e0b4c692852efc2683c38d72a12085
Richard Anaya <richard.anaya@gmail.com>
2011-03-17 12:18:35 -0500

    moving around, updating scratch

--- compile.bat
+++ compile.bat
@@ -2,5 +2,5 @@ cd src
 compile_templates.bat
 python compile.py
 cd ..
-cp src/SpriteEngine-min.js example/js/
-cp run_server.sh example/
+cp src/SpriteEngine-min.js examples/js/
+cp run_server.sh examples/
